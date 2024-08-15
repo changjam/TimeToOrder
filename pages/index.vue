61247b65c175ef2a0183f10b19ac7d40cfaa95b4
samfunmula <kobejames5566@gmail.com>
2024-08-15 09:56:36 +0800

    origin login format

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,54 @@
 <template>
   <h1>TimeToOrder</h1>
-  <p v-if="isAuthenticated">Hello, {{ userInfo.name }}!</p>
-  <button v-else="isAuthenticated" @click="login">登入</button>
+  <div v-if="isAuthenticated">
+    <p>Hello, {{ userInfo.name }}!</p><br>
+    <button @click="logout">登出</button>
+  </div>
+  <ClientOnly v-else>
+    <GoogleLogin :callback="callback" />
+  </ClientOnly>
 </template>
-  
-<script setup>
-  import { useRouter } from 'vue-router'
-  import { useState } from '#app'
 
-  const router = useRouter()
-  function login() {
-    router.push('/login')
+<script setup>
+  import { ref, onMounted } from 'vue'
+
+  const isAuthenticated = ref(false)
+  const userInfo = ref({ name: '', email: '' })
+
+  onMounted(() => {
+    const storedIsAuthenticated = sessionStorage.getItem('isAuthenticated') === 'true'
+
+    if (storedIsAuthenticated) {
+      isAuthenticated.value = true
+      userInfo.value.name = sessionStorage.getItem('user-name')
+      userInfo.value.email = sessionStorage.getItem('user-email')
+    }
+  })
+
+  const callback = (response) => {
+    // credential => JWT(JSON Web Token)
+    const { credential } = response;
+
+    if (credential) {
+      const data = JSON.parse(atob(credential.split('.')[1])); // atob => decode credential
+      sessionStorage.setItem('user-name', data.name)
+      sessionStorage.setItem('user-email', data.email)
+      sessionStorage.setItem('isAuthenticated', 'true')
+
+      userInfo.value.name = data.name
+      userInfo.value.email = data.email
+      isAuthenticated.value = true
+    } else {
+      alert('登入失敗')
+      sessionStorage.setItem('isAuthenticated', 'false')
+      isAuthenticated.value = false
+    }
   }
 
-  const userInfo = useState('userInfo')
-  const isAuthenticated = useState('isAuthenticated')
-  
+  const logout = () => {
+    sessionStorage.setItem('user-name', '')
+    sessionStorage.setItem('user-email', '')
+    sessionStorage.setItem('isAuthenticated', 'false')
+    isAuthenticated.value = false
+  }
 </script>
