288f5d98587af3baeff16ff74e1f95cda92aaf5c
張家誠 <changjam60@gmail.com>
2024-08-27 22:00:33 +0800

    feat: change model structure

--- server/models/Orders.js
+++ server/models/Orders.js
@@ -1,15 +1,18 @@
 import mongoose from 'mongoose';
 
 const OrderSchema = new mongoose.Schema({
-  restaurant: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurants', required: true },
-  groupIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Groups' }],
+  order_name: { type: String, required: true },
   deadline: Date ,
-  createdAt: { type: Date, default: Date.now },
-  creator: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
+  createdAt: { type: Date, default: Date.now, required: true },
+  order_open_time: { type: Date, default: Date.now, required: true },
+  order_lock_time: { type: Date, default: function() {return new Date(this.order_open_time.getTime() + 60 * 60 * 1000);}, required: true },
+  creator_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
+  restaurant_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurants', required: true },
+  group_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Groups', required: true },
   status: {
     type: String,
-    enum: ['Available', 'Locked', 'Finished'],
-    default: 'Available',
+    enum: ['Available', 'Locked', 'In_Progress', 'Finished', 'Canceled'],
+    default: 'Locked',
   },
   notes: { type: String, default: '' },
 });
