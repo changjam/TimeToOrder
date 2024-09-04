fd8d654ef2a2ec613a88f49641bf87e5fca1fcd0
張家誠 <changjam60@gmail.com>
2024-09-04 22:35:57 +0800

    restaurant id

--- pages/restaurants/[id].vue
+++ pages/restaurants/[id].vue
@@ -9,36 +9,51 @@ import { updateOrder } from '~/utils/order/orderHandler';
 const route = useRoute();
 const router = useRouter();
 
-const _id = route.fullPath.match(/\/restaurants\/(.+)?/)[1];
+// const _id = route.fullPath.match(/\/restaurants\/(.+)?/)[1];
 const menu_data = ref(null);
-const classifiedMenu = ref({});
+// const classifiedMenu = ref({});
 const classifiedDishes = ref(null)
-var categories = ref(null);
-var order_info = ref(null); // 訂購資訊
+// var categories = ref(null);
 const order_id = useState('order_id')
 const user_info = ref('')
+const categories = ref(null);
+const userOrder = ref({});
+const orderResult = ref(null);
+
+onMounted(async () => {
+  const data = await verify_credential()
+  if (!data) 
+    router.push('/login');
+
+  const user_id = data.user_id
+  user_info.value = await getUserData(`user_id=${user_id}`);
+  const restaurant_id = route.fullPath.match(/\/restaurants\/(.+)/)[1];
+  await fetchMenus(restaurant_id);
+});
+
+const isChanged = computed(() => {
+  for (const [key, dish] of Object.entries(userOrder.value)) {
+    const { amount, remark } = dish;
+    if(amount)
+      return true
+  }
+  return false;
+});
 
 const fetchMenus = async (restaurantId) => {
     try {
       menu_data.value = await getMenus(`restaurant=${restaurantId}`);
       classifiedDishes.value = classifyDishes(menu_data.value.data);
-      categories = Object.keys(classifiedDishes.value);
+      categories.value = Object.keys(classifiedDishes.value);
+      menu_data.value.data.forEach((dish)=>{
+        const { _id, ...data } = dish;
+        userOrder.value[_id] = {...data, amount: 0, remark: ""};
+      })
+
     }catch(error) {
-      console.log(error)
+      console.error(error)
     }
 };
-
-onMounted(async () => {
-  const data = await verify_credential()
-  const user_id = data.user_id
-  user_info.value = await getUserData(`user_id=${user_id}`);
-  classifiedMenu.value = await fetchMenus(_id);
-  order_info.value = menu_data.value.data
-  order_info.value.forEach(obj => {
-    obj.mount = NaN;
-    obj.note = "";
-  });
-});
 
 // 後續處理...
 onBeforeUnmount(() => {
@@ -49,61 +64,11 @@ const goBack = () => {
   router.push('/restaurants');
 };
 
-const resultWrapper = ref(null);
 
-const test = (dish) => {
-  const volumeElement = document.getElementById(`volume-${dish.name}`);
-  const remarkElement = document.getElementById(`remark-${dish.name}`);
+const send_order = async () => {
+  if (!isChanged)
+    return;
 
-  const volume = volumeElement ? volumeElement.value : 0;
-  const remark = remarkElement ? remarkElement.value : '';
-
-  // 暫時刪除 直接用order_info判斷
-  // if (volume == 0){
-  //   alert('數量必須大於0');
-  //   return
-  // }
-  const tr = document.createElement('tr');
-  for ( const order of order_info.value ){
-    if ( order._id == dish._id ){
-      order.mount = volume;
-      order.note = remark;
-    }
-  }
-  
-  if (resultWrapper.value) {
-    resultWrapper.value.innerHTML = '';
-    order_info.value
-      .filter(order => order.mount > 0)
-      .forEach(order => {
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
-          <th scope="row" class="cell">${order.name}</th>
-          <td class="price" class="cell">${order.price}</td>
-          <td align="right" class="cell">${order.mount}個</td>
-          <td align="right" class="cell">${order.note}</td>
-        `;
-        resultWrapper.value.appendChild(tr);
-      });
-  }
-
-  if (volumeElement) volumeElement.value = 0;
-  if (remarkElement) remarkElement.value = '';
-}
-
-const ordering = async () => {
-  const orders = order_info.value.filter(order => order.mount > 0);
-
-  if (!orders.length){
-    alert('請選擇餐點');
-    return
-  }
-
-  if (!order_id.value){
-    alert('請先至訂單頁面選擇訂單')
-    router.push('/order')
-    return
-  }
   const orderDetails = {
     name: user_info.value.data.name,
     totalAmount: NaN,
@@ -113,11 +78,11 @@ const ordering = async () => {
   }
 
   let total = 0;
-  for (const order of orders){
-    total += order.mount * order.price;
+  for (const [key, dish] of Object.entries(userOrder.value)){
+    total += dish.amount * dish.price;
     orderDetails.orderedItems.push({
-      itemName: order.name,
-      quantity: order.mount
+      itemName: dish.name,
+      quantity: dish.amount
     });
   }
   orderDetails.totalAmount = total;
@@ -130,62 +95,70 @@ const ordering = async () => {
     alert("訂單送出失敗")
   }
 }
-
 </script>
 
 
 <template>
     <div class="restaurant-wrapper">
       <div class="menu-wrapper">
-        <h2>菜單</h2>
+        <h2 class="header-title">菜單</h2>
         <table>
           <thead>
             <tr>
-              <th scope="col" class="name">餐點</th>
-              <th scope="col" class="price">價錢</th>
-              <th scope="col" class="num">數量</th>
-              <th scope="col" class="remark">備註</th>
-              <th scope="col"></th>
+              <th scope="col" class="name"><h3>餐點</h3></th>
+              <th scope="col" class="price"><h3>價錢</h3></th>
+              <th scope="col" class="amount"><h3>數量</h3></th>
+              <th scope="col" class="remark"><h3>備註</h3></th>
             </tr>
           </thead>
           <tbody v-if="menu_data">
-            <template v-for="category in categories" :key="category">
-              <tr class="dish.category">
-                <th colspan="1">{{ category }}</th>
-                <th colspan="4"></th>
+            <template v-for="category, idx_1 in categories" :key="idx_1">
+              <tr class="dish-category">
+                <th colspan="4">
+                  <h3 class="text">{{ category }}</h3>
+                </th>
               </tr>
-              <tr v-for="dish in classifiedDishes[category]" :key="dish.name">
-                <th scope="row" class="name">{{ dish.name }}</th>
-                <td class="price">{{ dish.price }}</td>
-                <td align="right">
-                  <input type="number" :id="`volume-${dish.name}`" value="0" min="0" class="no-arrows">
+              <tr class="dish-list" v-for="dish, idx_2 in classifiedDishes[category]" :key="idx_2">
+                <td class="field name">
+                  <h3>
+                    {{ dish.name }}
+                  </h3>
                 </td>
-                <td align="right">
-                  <input type="text" :id="`remark-${dish.name}`">
+                <td class="field price">
+                  <h3>
+                    {{ dish.price }}
+                  </h3>
                 </td>
-                <td>
-                  <button @click="test(dish)">確認</button>
+                <td class="field amount">
+                  <input class="input-field" type="number" v-model="userOrder[dish._id].amount" min="0">
+                </td>
+                <td class="field remark">
+                  <input class="input-field" type="text" v-model="userOrder[dish._id].remark">
                 </td>
               </tr>
             </template>
+            <tr class="send-btn-container" v-if="isChanged">
+              <th colspan="4">
+                <button class="send-btn" @click="send_order">送出</button>
+              </th>
+            </tr>
           </tbody>
         </table>
       </div>
       <div class="result-wrapper">
-        <h2>訂購資訊</h2>
+        <h2 class="header-title">訂購資訊</h2>
         <table>
           <thead>
             <tr>
               <th scope="col" class="name">餐點</th>
               <th scope="col" class="price">價錢</th>
-              <th scope="col" class="num">數量</th>
+              <th scope="col" class="amount">數量</th>
               <th scope="col" class="remark">備註</th>
             </tr>
           </thead>
-          <tbody ref="resultWrapper">
-        
+        {{ userOrder }}
+          <tbody ref="orderResult">
           </tbody>
-          <button class="order-btn" @click="ordering">確認訂購</button>
         </table>
       </div>
     </div>
@@ -201,13 +174,10 @@ const ordering = async () => {
     padding-top: 1rem;
     gap: 1rem;
 }
-
-.restaurant-wrapper  h2{
+.restaurant-wrapper  .header-title{
     text-align: center;
     margin-bottom: 1rem;
 }
-
-
 .restaurant-wrapper table {
     border-collapse: collapse;
     border: 2px solid rgb(140 140 140);
@@ -223,32 +193,64 @@ const ordering = async () => {
     padding: 0.5rem;
 }
 
-.restaurant-wrapper th>*,
-.restaurant-wrapper td>* {
-    width: 100%;
+.menu-wrapper{
+  font-weight: 700;
+  text-align: center;
 }
-
-.menu-wrapper input[type="number"] {
-    text-align: end;
-    display: block;
-    max-width: 5rem;
+.menu-wrapper thead{
+  text-align: center;
+  font-weight: 900;
 }
-
-.menu-wrapper input[type="text"] {
-    text-align: right;
+.menu-wrapper tbody .dish-category{
+  background-color: #f4f4f4;
 }
-
-.menu-wrapper .name {
-    text-align: left;
-}
-
-.menu-wrapper .price,
-.menu-wrapper .num {
-    text-align: right;
-}
-
 .menu-wrapper .category{
     font-size: 1.2rem;
+}
+.dish-list .field{
+  padding: 0;
+  height: 40px;
+}
+.dish-list .field.name{
+  width: 40%;
+}
+.dish-list .field.price{
+  width: 10%;
+}
+.dish-list .field.amount{
+  width: 10%;
+}
+.dish-list .field.remark{
+  width: 40%;
+}
+.dish-list .field .input-field{
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  border: none;
+  background: none;
+  font-size: 16px;
+}
+
+.send-btn-container{
+  width: 100%;
+  height: 30px;
+}
+.send-btn-container th{
+  height: 30px;
+  padding: 0;
+}
+.send-btn-container .send-btn{
+  font-size: 18px;
+  font-weight: 700;
+  padding: 8px;
+  background-color: #f4f4f4;
+  width: 100%;
+  height: 100%;
+}
+.send-btn-container .send-btn:hover{
+  background-color: #eeeeee;
+  cursor: pointer;
 }
 
 .result-wrapper {
